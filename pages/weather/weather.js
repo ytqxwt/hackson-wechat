bee29e18277c85b01fcbe06a5e1497593729cac6
Toky <466350259@qq.com>
2018-06-02 18:56:46 +0800

    toky

--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -1,5 +1,6 @@
 // pages/weather/weather.js
 var app = getApp();
+var util = require("../../utils/util.js");
 //获取OPENID相关
 const APP_ID = 'wxb32df375196aa830';//输入小程序appid  
 const APP_SECRET = 'a4c5e15e2f612d3cf1ed126acdff6001';//输入小程序app_secret  
@@ -7,14 +8,39 @@ var OPEN_ID = ''//储存获取到openid
 var SESSION_KEY = ''//储存获取到session_key 
 Page({
   data: {
-    array: ['老爸', '老妈', '弟弟', '姐姐'],
+    relationArray: ['老爸', '老妈', '弟弟', '姐姐'],
+    cityArray: ['北京', '金华', '成都'],
+    weatherInfo: [],
     openId: null,
+    region: ['广东省', '广州市', '海珠区'],
+    customItem: ''
   },
-  onLoad: function (options) {
+  onLoad: function () {
     var that = this
     getOpenIdTap(that);
+    for (var i = 0; i < this.data.cityArray.length; i++) {
+      util.getWeatherData(this.data.cityArray[i], function (wd) {
+        var _list = that.data.weatherInfo;
+        _list[i] = wd;
+        that.setData({
+          wd: wd,
+          weatherInfo: _list,
+        });
+        console.log(wd);
+        console.log(that.data.weatherInfo);
+
+      });
+    }
+
+
+  },
+  onShow: function () {
+
+  },
+
+  addWeather: function () {
     wx.request({
-      url: '',//查询关系所需要的接口
+      url: '',//查询关系数组所需要的接口
       method: 'POST',
       header: {
         'content-type': 'application/x-www-form-urlencoded'
@@ -24,18 +50,12 @@ Page({
       },
       success: (res) => {
         console.log(res)
-        if (res.data.result == true) {
-          wx.navagat
-          wx.showToast({
-            title: '提交成功！',
-            icon: 'success',
-          })
-        } else {
-          wx.showToast({
-            title: `提交失败${res.data.massage}`,
-            image: '/images/error.png',
+        if (res.data != []) {
+          that.setData({
+            relationArray: res.data
           })
         }
+
       },
       fail: (e) => {
         console.log(`${e}`),
@@ -47,13 +67,6 @@ Page({
 
     })
   },
-  onShow: function () {
-
-  },
-
-  addWeather: function () {
-
-  },
   bindPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
@@ -61,7 +74,7 @@ Page({
     })
   },
   weatherSubmit: function (e) {
-    //console.log("这是上传签到信息所需要的信息" + "::" + this.data.cChooseId + "::" + this.data.cTime + startTime + "::" + endTime);
+    //console.log("这是信息所需要的" + "::" + this.data.cChooseId + "::" + this.data.cTime + startTime + "::" + endTime);
     wx.request({
       url: '',//上传关系以及城市的URL
       method: 'POST',
@@ -95,7 +108,14 @@ Page({
       }
 
     })
+  },
+  bindRegionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
   }
+
 })
 /*
 * 获取openid的
